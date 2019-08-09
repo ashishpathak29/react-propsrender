518f8dad322ae371bf8367f2a0c0f5ae52f2c063
ashishpathak29 <ashishpathakiit@gmail.com>
2019-08-09 12:43:28 +0530

    2 files modified

--- Components/State.js
+++ Components/State.js
@@ -1,7 +1,20 @@
 import React from 'react';
 class State extends React.Component{
+constructor(props){
+  super(props);
+  this.state = {
+
+    ...this.props.initial
+  }
+
+  this.setState = this.setState.bind(this)
+}
+
   render(){
-    return this.props.initial
+    return this.props.children({
+      state: this.state,
+      setState: this.setState
+    })
   }
 }
 export default State
--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component,Fragment } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
-import React, { Component, Fragment } from 'react'
 import State from './Components/State';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
 
+class App extends Component {
   render() {
     return (
       <Fragment>
-      <State initial={{loading: false, data: null}}>
-         {() => {
-           return <div>Heelo</div>
-         }}
+      <State initial={{loading: false, name:"ashish"}}>
+      {({state, setState})=>{
+        return <div>{JSON.stringify(state)}
+        <button onClick={()=>setState({loading:true, name:"NTT DATA"})}>Toggle button</button>
+        </div> 
+      }}
+      </State>
+      <State initial={{count:0}}>
+      {({state, setState})=>{
+        return <div>{JSON.stringify(state)}
+        <button onClick={()=>setState({count: state.count + 1})}>Count Button</button>
+        </div> 
+      }}
       </State>
       </Fragment>
     );
